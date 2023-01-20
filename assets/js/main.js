0f4c003e310c59199001f4a34e37b74591df90b4
Salvador Espinosa <espinosalvador94@outlook.com>
2023-01-19 21:26:08 -0600

    added some tailwind to calendar and team

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// ! small area where we can do a quick search
+
+function search() {
+  var searchValue = document.getElementById("searchInput").value;
+  console.log("Searching for: " + searchValue);
+}
