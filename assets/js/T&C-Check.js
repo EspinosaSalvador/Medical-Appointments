a062b2f7ce241810c4f233f6918cdd8560fa996f
Salvador Espinosa <espinosalvador94@outlook.com>
2023-01-25 21:16:22 -0600

    encouter bug with JSON

---
+++ assets/js/T&C-Check.js
@@ -0,0 +1,24 @@
+var initPage = document.querySelector(".Accept-Conditions");
+var checkBox = document.querySelector("#checkbox");
+var submitCheckedBtnValue = document.querySelector("#Checked-Btn-Value");
+
+checkBox.addEventListener("change", () => {
+  event.preventDefault();
+  if (checkBox.value == "true") {
+    checkBox.value = "false";
+  } else {
+    checkBox.value = "true";
+  }
+});
+
+submitCheckedBtnValue.addEventListener("click", () => {
+  event.preventDefault();
+  if (checkBox.value == "true") {
+    localStorage.setItem("T&C", "true");
+    window.location.replace("../../assets/HTML/Diagnoster.html");
+  } else {
+    var initError = document.createElement("p");
+    initError.textContent = "You need to accept the usage terms";
+    initPage.appendChild(initError);
+  }
+});
